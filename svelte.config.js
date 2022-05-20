1b7f5d3d571ec06ad60b98b840087dd09f6e9ae1
Askar Yusupov <devex.soft@gmail.com>
2022-05-20 16:58:21 +0500

    build(esbuild plugins): add @esbuild-plugin: node-global-polyfill, node-modules-polyfill

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,7 @@
 import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
+import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -10,7 +12,22 @@ const config = {
 	kit: {
 		adapter: adapter(),
 		vite: {
-			build: { target: 'esnext' }
+			build: { target: 'esnext' },
+			optimizeDeps: {
+				esbuildOptions: {
+					// Node.js global to browser globalThis
+					define: {
+						global: 'globalThis'
+					},
+					// Enable esbuild polyfill plugins
+					plugins: [
+						NodeModulesPolyfillPlugin(),
+						NodeGlobalsPolyfillPlugin({
+							buffer: true
+						})
+					]
+				}
+			}
 		}
 	}
 };
