02f6e34d0a9075bf710bf62b192ba8da4a3efe6b
Askar Yusupov <devex.soft@gmail.com>
2022-05-23 13:07:54 +0500

    feat: add connect/disconnect Metamask

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,30 +1,59 @@
 <script lang="ts">
+	import type { WebBundlr } from '@bundlr-network/client';
+
 	import type { providers } from 'ethers';
 
 	import { onMount } from 'svelte';
-	import { connected, provider, defaultEvmStores } from 'svelte-ethers-store';
+	import {
+		connected,
+		provider,
+		defaultEvmStores,
+		signer,
+		signerAddress
+	} from 'svelte-ethers-store';
 
+	let bundlr: WebBundlr;
 	const initialiseBundlr = async (p: providers.Provider) => {
 		const WebBundlr = (await import('@bundlr-network/client')).WebBundlr;
-		const bundlr = new WebBundlr('https://node1.bundlr.network', 'bnb', p);
+		bundlr = new WebBundlr('https://node1.bundlr.network', 'bnb', p);
 		await bundlr.ready();
 
-		console.log(bundlr);
-		return bundlr; // done!
+		bundlr.createTransaction;
+
+		console.log('address', bundlr.address);
+
+		const loadedBalance = await bundlr.getLoadedBalance();
+		console.log('loadedBalance', loadedBalance.toString());
+
+		const balance = await bundlr.getBalance($signerAddress);
+		console.log('balance', balance.toString());
 	};
 
-	onMount(async () => {
-		await defaultEvmStores.setProvider();
-		if ($connected && $provider) {
-			console.log($provider);
-			initialiseBundlr($provider);
-		}
-	});
+	// onMount(async () => {
+	// 	await defaultEvmStores.setProvider();
+	// 	if ($connected && $provider) {
+	// 		console.log($provider);
+	// 		initialiseBundlr($provider);
+	// 	}
+	// });
+
+	function connectMetamask() {
+		console.log('connect metamask');
+		return defaultEvmStores.setProvider();
+	}
+	function disconnectMetamask() {
+		console.log('disconnect metamask');
+		return defaultEvmStores.disconnect();
+	}
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
 <div>
-	metamask connected: {$connected}
+	{#if $connected}
+		<button on:click={disconnectMetamask}>Disconnect Metamask</button>
+	{:else}
+		<button on:click={connectMetamask}>Connect Metamask</button>
+	{/if}
 </div>
