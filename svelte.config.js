9765684b3f47223391042f80690ea22748bcf0a0
Askar Yusupov <devex.soft@gmail.com>
2022-05-26 11:41:45 +0500

    build: add node-stdlib-browser

--- svelte.config.js
+++ svelte.config.js
@@ -3,6 +3,9 @@ import preprocess from 'svelte-preprocess';
 import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
 import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill';
 
+import stdLibBrowser from 'node-stdlib-browser';
+import plugin from 'node-stdlib-browser/helpers/esbuild/plugin';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
@@ -19,14 +22,19 @@ const config = {
 			},
 			optimizeDeps: {
 				esbuildOptions: {
+					inject: ['node_modules/node-stdlib-browser/helpers/esbuild/shim.js'],
 					define: {
-						global: 'globalThis'
+						// global: 'globalThis'
+						global: 'global',
+						process: 'process',
+						Buffer: 'Buffer'
 					},
 					plugins: [
-						NodeGlobalsPolyfillPlugin({
-							buffer: true
-						}),
-						NodeModulesPolyfillPlugin()
+						// NodeGlobalsPolyfillPlugin({
+						// 	buffer: true
+						// }),
+						// NodeModulesPolyfillPlugin()
+						plugin(stdLibBrowser)
 					]
 				}
 			}
