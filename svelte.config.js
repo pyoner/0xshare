c046effa6fbb8d893a863a6df7ab289a5032f259
Askar Yusupov <devex.soft@gmail.com>
2022-06-22 10:59:06 +0500

    fix: update deps

--- svelte.config.js
+++ svelte.config.js
@@ -2,7 +2,10 @@ import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
 import stdLibBrowser from 'node-stdlib-browser';
-import plugin from 'node-stdlib-browser/helpers/esbuild/plugin';
+import inject from'@rollup/plugin-inject';
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -18,18 +21,38 @@ const config = {
 			build: {
 				target: 'esnext'
 			},
-			optimizeDeps: {
-				esbuildOptions: {
-					inject: ['node_modules/node-stdlib-browser/helpers/esbuild/shim.js'],
-					define: {
-						// global: 'globalThis'
-						global: 'global',
-						process: 'process',
-						Buffer: 'Buffer'
-					},
-					plugins: [plugin(stdLibBrowser)]
-				}
+
+			resolve: {
+			alias: stdLibBrowser
+		},
+		optimizeDeps: {
+			include: ['buffer', 'process']
+		},
+		plugins: [
+			{
+				...inject({
+					global: [
+						require.resolve(
+							'node-stdlib-browser/helpers/esbuild/shim'
+						),
+						'global'
+					],
+					process: [
+						require.resolve(
+							'node-stdlib-browser/helpers/esbuild/shim'
+						),
+						'process'
+					],
+					Buffer: [
+						require.resolve(
+							'node-stdlib-browser/helpers/esbuild/shim'
+						),
+						'Buffer'
+					]
+				}),
+				enforce: 'post'
 			}
+		]
 		}
 	}
 };
